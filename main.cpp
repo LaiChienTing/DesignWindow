db91f66b514b2713fb2d538e8fae7beb5e0a5cc1
41143158 <41143158@nfu.edu.tw>
2025-12-08 13:54:26 +0800

    Project Created!

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+#include <QPalette>
+#include <QStyleFactory>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    // Set dark theme style
+    a.setStyle(QStyleFactory::create("Fusion"));
+
+    QPalette darkPalette;
+    darkPalette.setColor(QPalette::Window, QColor(53, 53, 53));
+    darkPalette.setColor(QPalette::WindowText, Qt::white);
+    darkPalette.setColor(QPalette::Base, QColor(42, 42, 42));
+    darkPalette.setColor(QPalette::AlternateBase, QColor(66, 66, 66));
+    darkPalette.setColor(QPalette::ToolTipBase, Qt::white);
+    darkPalette.setColor(QPalette::ToolTipText, Qt::white);
+    darkPalette.setColor(QPalette::Text, Qt::white);
+    darkPalette.setColor(QPalette::Button, QColor(53, 53, 53));
+    darkPalette.setColor(QPalette::ButtonText, Qt::white);
+    darkPalette.setColor(QPalette::BrightText, Qt::red);
+    darkPalette.setColor(QPalette::Link, QColor(42, 130, 218));
+    darkPalette.setColor(QPalette::Highlight, QColor(42, 130, 218));
+    darkPalette.setColor(QPalette::HighlightedText, Qt::black);
+
+    a.setPalette(darkPalette);
+
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
